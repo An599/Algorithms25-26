10c429b4525b3101e76bb74160c19052d03a7414
Anush Yesayan <70717954+An599@users.noreply.github.com>
2025-10-12 18:37:01 +0400

    ArrDeque.h

---
+++ Deque/ArrDeque/ArrDeque.h
@@ -0,0 +1,32 @@
+#pragma once
+
+#include "IDeque.h"
+#include <cstddef>
+#include <array>
+
+template <typename T, size_t N>
+class ArrDeque : public Deque<T>
+{
+public:
+	ArrDeque() noexcept;
+	~ArrDeque();
+
+	void push_back(const T&) override;
+	void push_back(T&&) override;
+	void pop_back() override;
+	void pop_front() override;
+	void push_front(const T&) override;
+	void push_front(T&&) override;
+	bool empty() const noexcept override;
+	bool full() const noexcept;
+	std::size_t size() const noexcept override;
+	const T& front() const override;
+	const T& rear() const override;
+	void clear() override;
+	void swap(ArrDeque&) noexcept;
+private:
+	std::size_t count , f_index, r_index;
+	std::array<T, N> arr; 
+};
+
+#include "ArrDeque.ipp"
