b3bf86e90489c839f5fff4674aa3df1f817918e8
Anush Yesayan <70717954+An599@users.noreply.github.com>
2025-11-30 17:22:11 +0400

    Update hanoi_tower.cpp

--- hanoi_tower.cpp
+++ hanoi_tower.cpp
@@ -6,26 +6,33 @@ void arrange_tow(int n, std::stack<int>& s1, std::stack<int>& s2, std::stack<int
     if (n == 0) {
         return;
     }
-    arrange_tow(n - 1, s1, s3, s2); //s1i vric n-1 haty qcuma s3
-    s2.push(s1.top());
+    arrange_tow(n - 1, s1, s3, s2);        
+    s2.push(s1.top());                    
     s1.pop();
-    arrange_tow(n - 1, s3, s2, s1); //s3 iny qcuma s2 i vra 
+    arrange_tow(n - 1, s3, s2, s1);        
 }
 
 int main()
 {
     std::stack<int> s1, s2, s3;
-    int n = 5;
-    s1.push(5);
-    s1.push(4);
-    s1.push(3);
-    s1.push(2);
-    s1.push(1);
+    int n;
+
+    std::cout << "Enter number of disks: ";
+    std::cin >> n;
+    std::cout << std::endl;
+
+    for (int i = n; i >= 1; i--) {
+        s1.push(i);
+    }
+
     arrange_tow(n, s1, s2, s3);
+
+    std::cout << "Final tower (s2): " <<std::endl;
     while (!s2.empty())
     {
         std::cout << s2.top() << std::endl;
         s2.pop();
     }
 
+    return 0;
 }
